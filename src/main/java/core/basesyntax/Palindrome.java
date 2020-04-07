7a1bdfe81c373ada5fcfabc1a96f8a0539af4dd0
Oleksandr58 <kovaliukoleksandr@gmail.com>
2020-04-07 09:10:42 +0300

    isPalindrome method in Palindrome class

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,9 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        text = text.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        String textFormatted = new StringBuilder(text).reverse().toString();
+
+        return text.equals(textFormatted);
     }
 }
